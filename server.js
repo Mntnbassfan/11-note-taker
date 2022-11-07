22d892ee2f35607224de3eff0367ac80fbfcb9ea
Mntnbassfan <108299929+Mntnbassfan@users.noreply.github.com>
2022-11-07 12:13:27 -0700

    added post routes

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const { application } = require("express");
 const express = require("express");
 const path = require("path");
+const { receiveMessageOnPort } = require("worker_threads");
 const api = require("./routes/index.js");
 
 const PORT = 3001;
 
 const app = express();
+// uuid to create an id for notes
+const { uuid } = require('path')
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const notes = (.db/db.json);
+const notes = (/db/db.json);
 app.use("/api", api);
 
 app.use(express.static("public"));
@@ -22,16 +25,27 @@ app.get("/", (req, res) => {
 
 // GET Route for notes page
 app.get("/notes", (req, res) => {
-  res.sendfile(path.join(__dirname, "public/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
 // GET Route for notes
-app.get('api/notes', (req, res) =>{
+app.get('api/notes', (req, res) => {
     res.json(notes)
 });
 
 // POST Route for notes
+app.post('api/notes', (req, res) => {
+    let nextNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid(),
+    }
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+//Push notes to json file
+notes.push(nextNote)
+//Send success message
+res.json(200)
+
+// function to delete notes
+
+});
